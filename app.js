1c7efaa2ed7d6e5469d17718a44b0b518e4f21e5
Juggy-45 <ahmedburhan.lm10@gmail.com>
2023-07-24 15:49:34 +0500

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const Test = require('./modules/test.controller')
-Test.solution();
+const express = require('express');
+const Test = require('./modules/test.controller');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const result = Test.solution();
+    res.json(result);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
